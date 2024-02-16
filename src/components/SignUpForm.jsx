fe6454f9822b4185429098b35ad5c4596ea1b65b
atloyd <aloyd@AustinsPC>
2024-02-15 19:39:32 -0600

    first commit

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+export default function SignUpForm() {
+	const [username, setUserName] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState(null);
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+
+		try {
+			const response = await fetch(
+				'https://fsa-jwt-practice.herokuapp.com/signup',
+				{
+					method: 'POST',
+					body: JSON.stringify({ username, password }),
+				}
+			);
+
+			const result = await response.json();
+			console.log(result);
+
+			const token = setToken(result.token)
+
+		} catch (error) {
+			setError(error.message);
+		}
+	}
+
+	return (
+		<>
+			<h2>Sign Up Here!</h2>
+			{error && <p>{error}</p>}
+
+			<form onSubmit={handleSubmit}>
+				<label>
+					Username:{' '}
+					<input
+						type='text'
+						value={username}
+						onChange={(e) => setUserName(e.target.value)}
+					/>
+				</label>
+				<label>
+					Password:{' '}
+					<input
+						type='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</label>
+				<button>Submit</button>
+			</form>
+		</>
+	);
+}
